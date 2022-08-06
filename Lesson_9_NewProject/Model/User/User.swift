d0d3abc5f917c8a8125d90096dfdba5f384645b8
Dima Hubenia <dima96gubenya@gmail.com>
2022-08-06 20:41:45 +0300

    FirstSettings

---
+++ Lesson_9_NewProject/Model/User/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Lesson_9_NewProject
+//
+//  Created by Дима Губеня on 06.08.2022.
+//
+
+import Foundation
+
+
+struct User {
+    let login: String
+    let password: String
+}
